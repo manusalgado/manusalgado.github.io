afb5b00c7b79e5b9c5e427da65abf31901e34623
DESKTOP-UKLKRC8\IMAGINAMOS <mest.sal18@gmail.com>
2018-09-28 15:41:26 -0500

    Added weather app

---
+++ weather-app/src/components/Form.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+const Form = props => (
+  <form onSubmit={props.getWeather}>
+        <input type="text" placeholder="City..." name="city"/>
+        <input type="text" placeholder="Country..." name="country"/>
+        <button>Get weather</button>
+    </form>
+)
+
+export default Form;
---
+++ weather-app/src/components/Title.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+const Title = () => (
+  <div>
+     <h1 className="title-container__title">Weather Finder</h1>
+     <p className="title-container__subtitle">Find out temperature, conditions and more...</p>
+  </div>
+)
+
+export default Title;
+ 
