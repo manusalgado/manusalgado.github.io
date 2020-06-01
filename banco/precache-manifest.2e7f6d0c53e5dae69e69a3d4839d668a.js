c2700ad8a7cd4262b240382639f13e24f0f1e63c
Manuel Salgado <manuel.salgado@rappi.com>
2020-05-31 21:55:40 -0500

    make it better

---
+++ banco/precache-manifest.2e7f6d0c53e5dae69e69a3d4839d668a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc6de93f886a0af196e197159a8ae659",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7df13623a6c9078c2e12",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "ee221920a973b4f9faf4",
+    "url": "/static/css/main.30cefcdf.chunk.css"
+  },
+  {
+    "revision": "7df13623a6c9078c2e12",
+    "url": "/static/js/2.2f5ae868.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2f5ae868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee221920a973b4f9faf4",
+    "url": "/static/js/main.553e2087.chunk.js"
+  },
+  {
+    "revision": "1b41008a9164fc709338",
+    "url": "/static/js/runtime-main.98981b02.js"
+  }
+]);
